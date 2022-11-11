c1c877b2b0b602535459c687132baea60e6a44da
Jimin Kang <jimin951218@gmail.com>
2022-11-11 10:44:13 +0900

    ADD: 1부터 50의 자연수 중 짝수를 구하는 함수

---
+++ Desktop/test/wecode/test.js
@@ -0,0 +1,11 @@
+function getEvenNumber() {
+    let evenArr = [];
+    for (let i = 1; i <= 50; i++) {
+      if (i % 2 === 0) {
+        evenArr.push(i);
+      }
+    }
+    return evenArr;
+  }
+
+  getEvenNumber();
